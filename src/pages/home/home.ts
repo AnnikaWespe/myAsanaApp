a6219e5a440d865eabffe035ab25f37298f6e611
DESKTOP-976SE5J\annika <annikawespe@gmail.com>
2017-11-16 17:39:04 +0100

    asdf

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -11,12 +11,59 @@ import * as firebase from 'firebase/app';
 })
 export class HomePage {
 
+	emailAdress;
+	password;
+
   constructor(public navCtrl: NavController,
-  	public afAuth: AngularFireAuth) {
+  	public afAuth: AngularFireAuth,
+  	private alertCtrl: AlertController) {
 console.log('in here');
   }
   loginWithGoogle() {
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  loginWithFacebook(){
+
+  }
+
+  loginWithEmail(){
+  	this.afAuth.auth.signInWithEmailAndPassword(this.emailAdress, this.password).then((response)=>{
+  		console.log(response);
+  	});
+  }
+
+  createAccountWithEmail(){
+  	 let alert = this.alertCtrl.create({
+    title: 'Sign up',
+    inputs: [
+      {
+        name: 'emailAdress',
+        placeholder: 'example@host.com'
+      },
+      {
+        name: 'password',
+        placeholder: 'Password',
+        type: 'password'
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Create account',
+        handler: data => {
+          this.afAuth.auth.createUserWithEmailAndPassword(data.emailAdress, data.password).then((response)=>{console.log(response)});
+      }
+  	}
+    ]
+  })
+  alert.present();
   }
   logout() {
     this.afAuth.auth.signOut();
